db598a26dcc039b33fb88ebf3ec6c4ca8a28da5c
Vasil <vasil.dimov91@gmail.com>
2021-03-21 12:24:11 +0200

    Add CSharp Fundamentals Tutorials

---
+++ 01.C#Fundamentals/12th BITWISE OPERATIONS(17.02.2020) - General Lecture/Bitwise-Operation/2. First Bit/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace _2._First_Bit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            string binary = Convert.ToString(n, 2).ToString();
+
+            char[] arr = binary.ToCharArray().Reverse().ToArray();
+
+            Console.WriteLine(arr[1]);
+        }
+    }
+}
---
+++ 01.C#Fundamentals/12th BITWISE OPERATIONS(17.02.2020) - General Lecture/Bitwise-Operation/4. Bit Destroyer/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _4._Bit_Destroyer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int p = int.Parse(Console.ReadLine());
+
+            string binary = Convert.ToString(n, 2).ToString();
+
+            List<char> list = binary.ToCharArray().ToList();
+
+            if (p < list.Count)
+            {
+                list[p] = '0'; 
+            }
+
+            binary = "";
+            list.Reverse();
+            foreach (var item in list)
+            {
+                binary += item;
+            }
+
+            int number = Convert.ToInt32(binary, 2);
+
+            Console.WriteLine(number);
+        }
+    }
+}
