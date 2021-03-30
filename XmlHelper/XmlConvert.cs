be6cc149fe911c30fa033b36ab0543fc5a18aed5
Vasil Dimov <vasil.dimov91@gmail.com>
2021-03-30 18:47:43 +0300

    Create XmlHelper Façade

---
+++ XmlHelper/XmlConvert.cs
@@ -0,0 +1,46 @@
+﻿namespace SoftJail.XmlHelper
+{
+    using System.IO;
+    using System.Text;
+    using System.Xml.Serialization;
+
+    public static class XmlConvert
+    {
+        public static T Deserialize<T>(string xmlString)
+            where T : class
+        {
+            var xmlRoot = new XmlRootAttribute("Officers");
+            var serializer = new XmlSerializer(typeof(T), xmlRoot);
+
+            using var stringReader = new StringReader(xmlString);
+            var result = serializer.Deserialize(stringReader) as T;
+
+            return result;
+        }
+
+        public static string Serialize<T>(string xmlRootName, T dto)
+            where T : class
+        {
+            var xmlRoot = new XmlRootAttribute(xmlRootName);
+
+            var serializer = new XmlSerializer(dto.GetType(), xmlRoot);
+
+            var sb = new StringBuilder();
+            var stringWriter = new StringWriter(sb);
+            serializer.Serialize(stringWriter, dto, GetEmptyNamespace());
+
+            var xmlOutput = sb.ToString();
+
+            return xmlOutput;
+        }
+
+        private static XmlSerializerNamespaces GetEmptyNamespace()
+        {
+            var namespaceOutput = new XmlSerializerNamespaces();
+
+            namespaceOutput.Add(string.Empty, string.Empty);
+
+            return namespaceOutput;
+        }
+    }
+}
