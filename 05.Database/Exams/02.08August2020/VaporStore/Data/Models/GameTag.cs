5140024e9520d9c4a50fb74e84bb08fde77eebd6
Vasil Dimov <vasil.dimov91@gmail.com>
2021-03-31 13:16:08 +0300

    Add Exam problem from 08 August 2020

---
+++ 05.Database/Exams/02.08August2020/VaporStore/Data/Models/GameTag.cs
@@ -0,0 +1,19 @@
+﻿namespace VaporStore.Data.Models
+{
+    public class GameTag
+    {
+        public int GameId { get; set; }
+
+        public Game Game { get; set; }
+
+        public int TagId { get; set; }
+
+        public virtual Tag Tag { get; set; }
+    }
+}
+
+/*•	GameId – integer, Primary Key, foreign key (required)
+•	Game – Game
+•	TagId – integer, Primary Key, foreign key (required)
+•	Tag – Tag
+*/
