80b478012069ba5da55d6e88f39b8c1da9783499
Vasil Dimov <vasil.dimov91@gmail.com>
2021-04-01 12:30:56 +0300

    Solve Data Export problem

---
+++ 05.Database/Exams/03.13Dec2019/BookShop/DataProcessor/ExportDto/BookXmlExportDto.cs
@@ -0,0 +1,15 @@
+﻿namespace BookShop.DataProcessor.ExportDto
+{
+    using System.Xml.Serialization;
+
+    [XmlType("Book")]
+    public class BookXmlExportDto
+    {
+        [XmlAttribute]
+        public int Pages { get; set; }
+
+        public string Name { get; set; }
+
+        public string Date { get; set; }
+    }
+}
--- 05.Database/Exams/03.13Dec2019/BookShop/DataProcessor/Serializer.cs
+++ 05.Database/Exams/03.13Dec2019/BookShop/DataProcessor/Serializer.cs
@@ -1,26 +1,62 @@
 ﻿namespace BookShop.DataProcessor
 {
     using System;
-    using System.Globalization;
-    using System.IO;
     using System.Linq;
-    using System.Text;
-    using System.Xml;
-    using System.Xml.Serialization;
-    using Data;
+
     using Newtonsoft.Json;
-    using Formatting = Newtonsoft.Json.Formatting;
+
+    using Data;
+    using ExportDto;
+    using XmlHelper;
+    using Data.Models.Enums;
+    using System.Globalization;
 
     public class Serializer
     {
         public static string ExportMostCraziestAuthors(BookShopContext context)
         {
-            return "TODO";
+            var authorsToExport = context.Authors
+                .Select(x => new
+                {
+                    AuthorName = $"{x.FirstName} {x.LastName}",
+                    Books = x.AuthorsBooks
+                        .OrderByDescending(y => y.Book.Price)
+                        .Select(y => new
+                        {
+                            BookName = y.Book.Name,
+                            BookPrice = y.Book.Price.ToString("F2")
+                        })
+                        .ToArray()
+                })
+                .ToArray()
+                .OrderByDescending(x => x.Books.Length)
+                .ThenBy(x => x.AuthorName)
+                .ToArray();
+
+            var jsonOutput = JsonConvert.SerializeObject(authorsToExport, Formatting.Indented);
+
+            return jsonOutput;
         }
 
         public static string ExportOldestBooks(BookShopContext context, DateTime date)
         {
-            return "TODO";
+            var booksToExport = context.Books
+                .Where(x => x.PublishedOn < date && x.Genre == Genre.Science)
+                .AsEnumerable()
+                .OrderByDescending(x => x.Pages)
+                .ThenByDescending(x => x.PublishedOn)
+                .Select(x => new BookXmlExportDto
+                {
+                    Pages = x.Pages,
+                    Name = x.Name,
+                    Date = x.PublishedOn.ToString("d", CultureInfo.InvariantCulture)
+                })
+                .Take(10)
+                .ToArray();
+
+            var xmlOutput = XmlConvert.Serialize("Books", booksToExport);
+
+            return xmlOutput;
         }
     }
 }
