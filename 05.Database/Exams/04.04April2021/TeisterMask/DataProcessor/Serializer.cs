b7ac204f0ef795fdf1fc352a241b312f4d17ef45
Vasil Dimov <vasil.dimov91@gmail.com>
2021-04-04 09:24:42 +0300

    Add Exam and solve data definition

---
+++ 05.Database/Exams/04.04April2021/TeisterMask/DataProcessor/Serializer.cs
@@ -0,0 +1,21 @@
+﻿namespace TeisterMask.DataProcessor
+{
+    using System;
+
+    using Data;
+
+    using Formatting = Newtonsoft.Json.Formatting;
+
+    public class Serializer
+    {
+        public static string ExportProjectWithTheirTasks(TeisterMaskContext context)
+        {
+            return "TODO";
+        }
+
+        public static string ExportMostBusiestEmployees(TeisterMaskContext context, DateTime date)
+        {
+            return "TODO";
+        }
+    }
+}
